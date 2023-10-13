dce885c5603701331c3e2d35e0f262da9e491a44
ANA CAPELETTI <anacapeletti01@gmail.com>
2023-10-13 08:50:14 -0300

    Função de export atualizada

--- projetoana/src/main/java/functions/Export.java
+++ projetoana/src/main/java/functions/Export.java
@@ -11,30 +11,30 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Export {
-	
-	public void exportExcel(List<List<float [][]>> listaListaKernel) throws IOException {
-
+	public void exportExcel(List<List<float[][]>> listaListaKernel) throws IOException {
 		Workbook workbook = new XSSFWorkbook(); // new HSSFWorkbook() for generating `.xls` file
 		List<Sheet> listaPlanilhas = new ArrayList<Sheet>();
-		for(int i = 0 ; i < listaListaKernel.size(); i++) {
-			Sheet sheet = workbook.createSheet("Lista" + i); //criando as planilhas de listas de kernels
+		for (int i = 0; i < listaListaKernel.size(); i++) {
+			Sheet sheet = workbook.createSheet("Lista" + i); // criando as planilhas de listas de kernels
 			listaPlanilhas.add(sheet);
+			int contador = 0;
 			for (int j = 0; j < listaListaKernel.get(i).size(); j++) {
-				for(int linha = 0 ; linha < listaListaKernel.get(i).get(j).length; linha++) {
-					sheet.createRow(j*listaListaKernel.get(i).size() + linha + j);
-					System.out.println("Num:\n" + (j*listaListaKernel.get(i).size() + linha + j));
-					for(int k = 0 ; k < listaListaKernel.get(i).get(j).length; k++) {
-						listaPlanilhas.get(i).getRow(j*listaListaKernel.get(i).size() + linha + j).createCell(k);
-						listaPlanilhas.get(i).getRow(j*listaListaKernel.get(i).size() + linha + j).getCell(k).setCellValue(listaListaKernel.get(i).get(j)[linha][k]);
+				for (int linha = 0; linha < listaListaKernel.get(i).get(j).length; linha++) {
+					sheet.createRow(contador);
+					for (int k = 0; k < listaListaKernel.get(i).get(j).length; k++) {
+						listaPlanilhas.get(i).getRow(contador).createCell(k).setCellValue(listaListaKernel.get(i).get(j)[linha][k]);
 					}
+					contador++;
 				}
 			}
 		}
-		
+
 		DecimalFormat formatter = new DecimalFormat("#0.00");
 		FileOutputStream fileOut = new FileOutputStream("ListaKernels.xlsx");
 		workbook.write(fileOut);
 		fileOut.close();
 
 	}
+
+
 }
